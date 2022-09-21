8da799fa226577d80280e49993f17037f78c581e
Bharanilikith <Bharanilikith@Bharani>
2022-09-21 09:46:40 +0530

    hi

--- src/synchronization/Test2.java
+++ src/synchronization/Test2.java
@@ -4,7 +4,11 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Test2 {
 
@@ -13,16 +17,24 @@ public class Test2 {
 WebDriver driver=new ChromeDriver();
 driver.manage().window().maximize();
 driver.get("https://www.instagram.com/");
-driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+WebDriverWait w=new WebDriverWait(driver,Duration.ofSeconds(10));
 
 
-driver.findElement(By.name("username")).sendKeys("qwertyuio");
+WebElement user = driver.findElement(By.name("username"));
+w.until(ExpectedConditions.visibilityOf(user)).sendKeys("qwertyu");
+
 driver.findElement(By.name("password")).sendKeys("qwertyui");
-driver.findElement(By.xpath("(//div[contains(@class,'qF0y9')])[4]")).click();
+
+WebElement button = driver.findElement(By.xpath("(//div[contains(@class,'qF0y9')])[4]"));
+w.until(ExpectedConditions.elementToBeClickable(button)).click();
+
+String title = driver.getTitle();
+w.until(ExpectedConditions.titleContains("instagram"));
 
 
-//driver.close();
-
+Thread.sleep(3000);
+driver.close();
 	}
 
 }
